e5b1a5ca6a516dc96bc27185b1d200f7ca43dab4
= <george.bentes@gmail.com>
2023-11-28 15:18:21 -0300

    add: workspace page basic components

--- lib/pages/workspaces_page.dart
+++ lib/pages/workspaces_page.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:grorange/components/grid_button.dart';
+import 'package:grorange/components/grid_options.dart';
+import 'package:grorange/components/page_title.dart';
 
-class WorkspacesPage extends StatelessWidget {
+class WorkspacesPage extends StatefulWidget {
   const WorkspacesPage({super.key});
 
   @override
+  State<WorkspacesPage> createState() => _WorkspacesPageState();
+}
+
+class _WorkspacesPageState extends State<WorkspacesPage> {
+
+  @override
   Widget build(BuildContext context) {
+    // TODO: Load info from database
+    List<Widget> buttons = List.empty(growable: true);
+    buttons.add(const GridButton(text: 'Home'));
+    
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome, user!'),
+        title: const Text(
+          'Welcome, George!', // TODO: Get value from state manager
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+        },
+        child: const Icon(Icons.add),
       ),
       body: Column(
         children: [
-          Text('Workspaces', style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 32,
-          ),)
+          const SizedBox(
+            height: 40,
+          ),
+          const PageTitle(title: 'Workspaces'),
+          const SizedBox(
+            height: 60,
+          ),
+          GridOptions(buttons: buttons),
         ],
-      ),
+      )
     );
   }
 }
