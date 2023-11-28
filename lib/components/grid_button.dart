71b8f47dfdca897606658dedbef501701e0a52c8
= <george.bentes@gmail.com>
2023-11-28 12:29:18 -0300

    add: component grid button

---
+++ lib/components/grid_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class GridButton extends StatelessWidget {
+  final String text;
+
+  const GridButton({required this.text, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(100, 0, 0, 0),
+            spreadRadius: 0,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 16,
+            fontFamily: 'Roboto',
+          ),
+        ),
+      ),
+    );
+  }
+}
