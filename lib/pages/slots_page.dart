f235a71c57cb4f5fe74d5d89825d6123e2bb093c
= <george.bentes@gmail.com>
2023-11-29 16:50:29 -0300

    add: slots page

---
+++ lib/pages/slots_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:grorange/components/grid_options.dart';
+import 'package:grorange/components/page_app_bar.dart';
+import 'package:grorange/components/page_title.dart';
+import 'package:grorange/components/grid_button.dart';
+
+class SlotsPage extends StatefulWidget {
+  const SlotsPage({super.key});
+
+  @override
+  State<SlotsPage> createState() => _SlotsPageState();
+}
+
+class _SlotsPageState extends State<SlotsPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Load info from database
+    List<Widget> buttons = List.empty(growable: true);
+    buttons.add(const GridButton(text: 'Fridge'));
+
+    return Scaffold(
+      appBar: const PageAppBar(title: 'Home'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.of(context).push(MaterialPageRoute(
+          //   builder: (context) => const AddWorkspacePage(),
+          // ));
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          const PageTitle(title: 'Slots'),
+          const SizedBox(
+            height: 60,
+          ),
+          GridOptions(buttons: buttons),
+        ],
+      ),
+    );
+  }
+}
