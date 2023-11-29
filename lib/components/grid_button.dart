5437b4e1cc7424c8b1e60342fdce146e58f35f97
= <george.bentes@gmail.com>
2023-11-29 18:06:02 -0300

    add: ontap callback function

--- lib/components/grid_button.dart
+++ lib/components/grid_button.dart
@@ -2,34 +2,40 @@ import 'package:flutter/material.dart';
 
 class GridButton extends StatelessWidget {
   final String text;
+  final Function onTap;
 
-  const GridButton({required this.text, super.key});
+  const GridButton({required this.text, required this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: const [
-          BoxShadow(
-            color: Color.fromARGB(100, 0, 0, 0),
-            spreadRadius: 0,
-            blurRadius: 4,
-            offset: Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            text,
-            style: const TextStyle(
-              fontSize: 16,
-              fontFamily: 'Roboto',
+    return GestureDetector(
+      onTap: (){
+        onTap();
+      },
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(100, 0, 0, 0),
+              spreadRadius: 0,
+              blurRadius: 4,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 16,
+                fontFamily: 'Roboto',
+              ),
             ),
           ),
         ),
