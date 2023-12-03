75ccccafeb7751cf75741f2b6a72fc716a2e3583
= <george.bentes@gmail.com>
2023-12-03 14:52:21 -0300

    add: create table for slots, save and findAll methods

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,4 +1,5 @@
 import 'package:grorange/database/dao/item_dao.dart';
+import 'package:grorange/database/dao/slot_dao.dart';
 import 'package:grorange/database/dao/workspace_dao.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
@@ -10,6 +11,7 @@ Future<Database> getDatabase() async {
     onCreate: (db, version) {
       db.execute(ItemDAO.createTable);
       db.execute(WorkspaceDAO.createTable);
+      db.execute(SlotDAO.createTable);
     },
     version: 1,
   );
---
+++ lib/database/dao/slot_dao.dart
@@ -0,0 +1,46 @@
+import 'package:grorange/database/app_database.dart';
+import 'package:grorange/models/slot.dart';
+import 'package:sqflite/sqflite.dart';
+
+class SlotDAO {
+
+  static const String _id = 'id';
+  static const String _name = 'name';
+  static const String _userId = 'user_id';
+  static const String _workspaceId = 'workspace_id';
+  static const String _insertDateTime = 'insert_date_time';
+  static const String _updateDateTime = 'update_date_time';
+  static const String _active = 'active';
+
+  static const String tableName = 'slots';
+
+  static const String createTable = 'CREATE TABLE $tableName ('
+      '$_id TEXT, '
+      '$_name TEXT, '
+      '$_userId TEXT, '
+      '$_workspaceId TEXT, '
+      '$_insertDateTime TEXT, '
+      '$_updateDateTime TEXT, '
+      '$_active INTEGER)';
+
+
+  Future<int> save(Slot slot) async {
+    final Database db = await getDatabase();
+    return db.insert(tableName, slot.toMap());
+  }
+
+  Future<List<Slot>> findAll() async {
+    final Database db = await getDatabase();
+    List<Map<String, dynamic>> rows = await db.query(tableName);
+
+    List<Slot> slots = List.empty(growable: true);
+    for(Map<String, dynamic> row in rows){
+      slots.add(Slot.fromMap(row));
+    }
+
+    return slots;
+  }
+
+
+
+}
