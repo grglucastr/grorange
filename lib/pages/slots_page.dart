4a6d868b28e94feb63ee9f413b92cbb1ac453cad
= <george.bentes@gmail.com>
2023-12-05 22:44:22 -0300

    add: add action to delete workspace

--- lib/pages/slots_page.dart
+++ lib/pages/slots_page.dart
@@ -1,33 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:grorange/components/grid_button.dart';
 import 'package:grorange/components/grid_empty.dart';
 import 'package:grorange/components/grid_options.dart';
 import 'package:grorange/components/loading.dart';
-import 'package:grorange/components/page_app_bar.dart';
+import 'package:grorange/components/page_app_bar_with_actions.dart';
 import 'package:grorange/components/page_title.dart';
-import 'package:grorange/components/grid_button.dart';
 import 'package:grorange/database/dao/slot_dao.dart';
+import 'package:grorange/database/dao/workspace_dao.dart';
 import 'package:grorange/models/slot.dart';
+import 'package:grorange/models/workspace.dart';
 import 'package:grorange/pages/add_slot_page.dart';
 import 'package:grorange/pages/slot_items_page.dart';
 
 class SlotsPage extends StatefulWidget {
-  const SlotsPage({super.key});
+  final Workspace workspace;
+
+  const SlotsPage({required this.workspace, super.key});
 
   @override
   State<SlotsPage> createState() => _SlotsPageState();
 }
 
 class _SlotsPageState extends State<SlotsPage> {
+  var workspaceDAO = WorkspaceDAO();
+
   @override
   Widget build(BuildContext context) {
     var dao = SlotDAO();
     return Scaffold(
-      appBar: const PageAppBar(title: 'Home'),
+      appBar: PageAppBarWithActions(title: widget.workspace.name!, actions: [
+        IconButton(
+          onPressed: () {
+            workspaceDAO.delete(widget.workspace.id!);
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.delete),
+        )
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => const AddSlotPage(),
-          )).then((value) => setState(() {}));
+          Navigator.of(context)
+              .push(MaterialPageRoute(
+                builder: (context) => const AddSlotPage(),
+              ))
+              .then((value) => setState(() {}));
         },
         child: const Icon(Icons.add),
       ),
@@ -61,8 +77,8 @@ class _SlotsPageState extends State<SlotsPage> {
     );
   }
 
-  Widget _handleFutureBuilderDone(BuildContext context,
-      AsyncSnapshot<List<Slot>> snapshot) {
+  Widget _handleFutureBuilderDone(
+      BuildContext context, AsyncSnapshot<List<Slot>> snapshot) {
     if (snapshot.hasData) {
       List<Slot> slots = snapshot.data!;
       return slots.isNotEmpty
