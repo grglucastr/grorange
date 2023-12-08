6a7b3450f2b72b044ad89a276eb7caf042faec4c
= <george.bentes@gmail.com>
2023-12-07 23:02:48 -0300

    add: upgrade in the appbar to handle custom back function

--- lib/components/page_app_bar_with_actions.dart
+++ lib/components/page_app_bar_with_actions.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class PageAppBarWithActions extends StatefulWidget implements PreferredSizeWidget {
+class PageAppBarWithActions extends StatefulWidget
+    implements PreferredSizeWidget {
   final String title;
   final List<IconButton> actions;
 
-  const PageAppBarWithActions({
+  Function? leadButtonAction;
+  bool showLeadingButton;
+
+  PageAppBarWithActions({
     required this.title,
     required this.actions,
+    this.leadButtonAction,
+    this.showLeadingButton = false,
     super.key,
   });
 
@@ -20,15 +26,41 @@ class PageAppBarWithActions extends StatefulWidget implements PreferredSizeWidge
 }
 
 class _PageAppBarWithActionsState extends State<PageAppBarWithActions> {
-
   @override
   Widget build(BuildContext context) {
 
+    if(widget.showLeadingButton){
+      return _appBarLeadingButton();
+    }
+    return _appBarNoLeadingButton();
+
+  }
+
+  AppBar _appBarNoLeadingButton() {
     return AppBar(
       title: Text(widget.title),
       backgroundColor: Colors.blue,
       foregroundColor: Colors.white,
       actions: widget.actions,
+    );
+  }
+
+  AppBar _appBarLeadingButton() {
+    return AppBar(
+      title: Text(widget.title),
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      actions: widget.actions,
+      automaticallyImplyLeading: false,
+      leading: IconButton(
+        onPressed: () {
+          widget.leadButtonAction!();
+        },
+        icon: const Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
