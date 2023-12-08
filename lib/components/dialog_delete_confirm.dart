9ea145ff8ef0cf3b88f46dd511aa5f1c9be82053
= <george.bentes@gmail.com>
2023-12-08 09:16:20 -0300

    change: refactor the dialog title to use a reusable title component

--- lib/components/dialog_delete_confirm.dart
+++ lib/components/dialog_delete_confirm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grorange/components/dialog_box_title.dart';
 
 class DialogDeleteConfirm extends StatelessWidget {
   final String title;
@@ -20,25 +21,7 @@ class DialogDeleteConfirm extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              width: double.maxFinite,
-              height: 60,
-              decoration: const BoxDecoration(
-                  color: Colors.deepOrangeAccent,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20))),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-            ),
+            DialogBoxTitle(title: title),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(description),
