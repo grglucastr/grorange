4cb5d4df3267d66fafe5e0e685a93af5a4e24b37
= <george.bentes@gmail.com>
2024-01-02 23:34:17 -0300

    add: user data conversion

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:grorange/models/base.dart';
 
 class User extends Base {
@@ -9,4 +11,32 @@ class User extends Base {
     super.insertDateTime,
     super.updateDateTime,
   );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'insert_date_time': insertDateTime,
+      'update_date_time': updateDateTime,
+      'active': active
+    };
+  }
+
+  static User fromMap(Map<String, dynamic> map) {
+    final User user = User(
+      map['id'],
+      map['name'],
+      map['insert_date_time'],
+      map['update_date_time'],
+    );
+    user.active = map['active'];
+    return user;
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  static User fromJson(String json){
+    Map<String, dynamic> jsonMap = jsonDecode(json);
+    return fromMap(jsonMap);
+  }
 }
